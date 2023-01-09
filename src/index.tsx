e10e321888176e202ab486c70294091cb799d918
William <wilpat456@gmail.com>
2023-01-09 01:57:39 +0000

    Uses react 18 root rendering

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from 'react-router-dom'
-
 import { App } from './App'
+import { BrowserRouter as Router } from 'react-router-dom'
 import './index.css'
 
-ReactDOM.render(
-  <Router>
-    <App />
-  </Router>
-  , document.getElementById('root')); 
+import { createRoot } from 'react-dom/client'
+const container = document.getElementById('root')
+const root = createRoot(container!)
+root.render(<Router>
+  <App />
+</Router>)
+
+
+// ReactDOM.render(
+//   <Router>
+//     <App />
+//   </Router>
+//   , document.getElementById('root')); 
